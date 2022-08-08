6282780e41a7b2c10fe05fd5829ccf6885ae2a63
Jake Burton <jburto94@yahoo.com>
2022-08-08 13:56:47 -0400

    added search filter

--- src/App.js
+++ src/App.js
@@ -2,38 +2,46 @@ import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const database = "https://my-json-server.typicode.com/jburto94/superhero-rolodex/superheroes";
+
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      superheroes: [
-        {
-          name: 'Superman',
-          id: 'bn4792jk'
-        },
-        {
-          name: 'Invincible',
-          id: '47hgbbjk'
-        },
-        {
-          name: 'Hulk',
-          id: 'asdfjb884r'
-        },
-        {
-          name: 'Thor',
-          id: 'ad7878fh7'
-        }
-      ]
+      superheroes: [],
+      filter: ''
     }
   }
 
+  componentDidMount() {
+    fetch(database)
+      .then(response => response.json())
+      .then(superheroes => this.setState({ superheroes: superheroes }))
+  }
+
+  onSearchChange = event => {
+    this.setState({ filter: event.target.value.toLowerCase() })
+  }
+
   render() {
+    const { filter, superheroes } = this.state
+    const { onSearchChange } = this
+
+    const filteredHeroes = superheroes
+      .filter(hero => hero.name.toLowerCase().includes(filter))
+
     return (
       <div className="App">
-        {this.state.superheroes.map(hero => 
-          <h1 key={hero.id}>{hero.name}</h1>  
-        )}
+        <input 
+          className='search-box'
+          type='search'
+          placeholder='search superheroes'
+          onChange={onSearchChange}
+        />
+        {filteredHeroes
+          .map(hero => <h1 key={hero.id}>{hero.name}</h1>)
+        }
       </div>
     );
   }
