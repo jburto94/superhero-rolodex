eaf0a294dcf1e60cd42cf371bb16abb31de62c75
Jake Burton <jburto94@yahoo.com>
2022-08-08 12:36:35 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      superheroes: [
+        {
+          name: 'Superman',
+          id: 'bn4792jk'
+        },
+        {
+          name: 'Invincible',
+          id: '47hgbbjk'
+        },
+        {
+          name: 'Hulk',
+          id: 'asdfjb884r'
+        },
+        {
+          name: 'Thor',
+          id: 'ad7878fh7'
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.superheroes.map(hero => 
+          <h1 key={hero.id}>{hero.name}</h1>  
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
